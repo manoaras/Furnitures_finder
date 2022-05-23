909cac2e94b820717a3047da42d71eb369c2184d
Jacques Antoine de Cugnac <ja.decugnac@outlook.com>
2022-05-23 17:35:47 +0200

    skip authencation for index & furnitures#show

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,5 +1,6 @@
 class FurnituresController < ApplicationController
 before_action :set_furniture, only: [:show, :destroy]
+skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @furnitures = Furniture.all
