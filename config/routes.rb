fcdfda670a9a80d8349bbd0a817908a66ce0d1de
antoine mendy <antoine.mendy@hotmail.fr>
2022-05-24 11:00:57 +0200

    create booking view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
-  resources :furnitures
+  resources :furnitures do
+    resources :bookings
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_154956) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_082443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "furniture_id"
+    t.integer "start_date"
+    t.integer "end_date"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "furnitures", force: :cascade do |t|
     t.string "description"
