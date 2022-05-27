ea32d3bf4d9d2934dd3414b5c6d3a5cba9492a56
Jeanne dL <jeanne.deleusse@gmail.com>
2022-05-27 12:37:24 +0200

    modifs jour en mois

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,139 +31,139 @@ randomize_user = [user1, user2]
 furnitures = [
   {
     description: "A clean look that’s easy to like and mix with other styles, either supporting a desk or standing alone. The back is finished so you can place it in the middle of the room – all sides are just as beautiful.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "couch",
     city: "Paris"
   },
   {
     description: "You know the feeling when you sit, lie down or hang out in a sofa, rather than on it. That’s how embracing the deep and generous UPPLAND sofa is – your new favorite place for cozy evenings and lazy days!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "couch",
     city: "Paris"
   },
   {
     description: "A clean look that’s easy to like and mix with other styles, either supporting a desk or standing alone. The back is finished so you can place it in the middle of the room – all sides are just as beautiful.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "chair",
     city: "Paris"
   },
   {
     description: "This stackable, 3-legged stool with bent legs and triangular-shaped seat is a great little accent that’s comes in handy when you need extra seating for guests or a place to unload your cup of coffee or tea!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "table",
     city: "Paris"
   },
   {
     description: "You know the feeling when you sit, lie down or hang out in a sofa, rather than on it. That’s how embracing the deep and generous UPPLAND sofa is – your new favorite place for cozy evenings and lazy days!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "stool",
     city: "Paris"
   },
   {
     description: "Comfy seating throughout the home gives a relaxed feel – and with airy PELLO armchair you can easily create your comfort zones everywhere. The secrets are good back support and a slightly resilient frame!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "sofa",
     city: "Paris"
   },
   {
     description: "Comfy seating throughout the home gives a relaxed feel – and with airy PELLO armchair you can easily create your comfort zones everywhere. The secrets are good back support and a slightly resilient frame!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "chair",
     city: "Paris"
   },
   {
     description: "Sleeper sofa offers around-the-clock comfort. The adjustable back allows you to sit during the day, lean back in the evening and lie down at night. The Vissle gray cover fits in just about anywhere.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "drawer",
     city: "Paris"
   },
   {
     description: "A clean expression that fits right in, in the bedroom or wherever you place it. Smooth-running drawers and in a choice of finishes – pick your favorite. Psst! Please attach to the wall.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "cupboard",
     city: "Paris"
   },
   {
     description: "You know the feeling when you sit, lie down or hang out in a sofa, rather than on it. That’s how embracing the deep and generous UPPLAND sofa is – your new favorite place for cozy evenings and lazy days!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "sofa",
     city: "Paris"
   },
   {
     description: "Comfy seating throughout the home gives a relaxed feel – and with airy PELLO armchair you can easily create your comfort zones everywhere. The secrets are good back support and a slightly resilient frame!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "lamp",
     city: "Bordeaux"
   },
   {
     description: "This table for 4 blends a warm wood expression with sturdy metal in a slim design that’s pleasing to the eye even in smaller spaces. Pair it with SANDSBERG chair to create a welcoming and coordinated look.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "couch",
     city: "Bordeaux"
   },
   {
     description: "This stackable, 3-legged stool with bent legs and triangular-shaped seat is a great little accent that’s comes in handy when you need extra seating for guests or a place to unload your cup of coffee or tea!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "sofa",
     city: "Bordeaux"
   },
   {
     description: "You know the feeling when you sit, lie down or hang out in a sofa, rather than on it. That’s how embracing the deep and generous UPPLAND sofa is – your new favorite place for cozy evenings and lazy days!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "couch",
     city: "Bordeaux"
   },
   {
     description: "You know the feeling when you sit, lie down or hang out in a sofa, rather than on it. That’s how embracing the deep and generous UPPLAND sofa is – your new favorite place for cozy evenings and lazy days!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "armchair",
     city: "Bordeaux"
   },
   {
     description: "A clean look that’s easy to like and mix with other styles, either supporting a desk or standing alone. The back is finished so you can place it in the middle of the room – all sides are just as beautiful.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "lamp",
     city: "Bordeaux"
   },
   {
     description: "This table for 4 blends a warm wood expression with sturdy metal in a slim design that’s pleasing to the eye even in smaller spaces. Pair it with SANDSBERG chair to create a welcoming and coordinated look.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "shelf",
     city: "Bordeaux"
   },
   {
     description: "Comfy seating throughout the home gives a relaxed feel – and with airy PELLO armchair you can easily create your comfort zones everywhere. The secrets are good back support and a slightly resilient frame!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "rug",
     city: "Bordeaux"
   },
   {
     description: "Sleeper sofa offers around-the-clock comfort. The adjustable back allows you to sit during the day, lean back in the evening and lie down at night. The Vissle gray cover fits in just about anywhere.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "armchair",
     city: "Bordeaux"
   },
   {
     description: "This stackable, 3-legged stool with bent legs and triangular-shaped seat is a great little accent that’s comes in handy when you need extra seating for guests or a place to unload your cup of coffee or tea!",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "oven",
     city: "Bordeaux"
   },
   {
     description: "A clean design with solid wood veneer. Place the bed on its own or with the headboard against a wall. If you need space for extra bedding, add MALM bed storage boxes on casters.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "couch",
     city: "Bordeaux"
   },
   {
     description: "The black stainless steel and space-saving design set the tone in the kitchen. There are many options to tailor the space inside for easy access and a good overview, putting an end to food waste at home.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "chair",
     city: "Paris"
   },
   {
     description: "This oven offers all the basic functions you need, including the convenience of self-clean. A look that fits right in and a smooth surface for easy cleaning.",
-    price_per_day: rand(1..10),
+    price_per_day: rand(10..20),
     name: "drawer",
     city: "Bordeaux"
   }
