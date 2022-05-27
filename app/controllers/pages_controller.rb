21aad658aceb4982c59c9ee8011c1541dc5134ab
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-27 10:11:03 +0200

    reverse add for new furnitures

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @furnitures = Furniture.last(6)
+    @furnitures = Furniture.last(6).reverse
   end
 end
